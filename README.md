0a44bc67c3819773e04309dee825d691c1e0755b
TranBaVinhSon <tranbavinhson@gmail.com>
2020-04-26 02:45:42 +0900

    Init AWS

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1 align="center">Welcome to terraform_training 👋</h1>
+<p>
+</p>
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+---
+
+## Usage
+
+Note: Terraform's version is 0.11
+
+```
+cd terraform_training/aws
+terraform init
+terraform apply
+```
---
+++ aws/example.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = "default"        # default profile in ~/.aws/config
+  region  = "ap-northeast-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0183ac312a029c38c"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = "${aws_instance.example.id}"
+}
