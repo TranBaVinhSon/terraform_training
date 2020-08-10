76ffb995bf62b61316c094f9a9e64f96b89f2df9
TranBaVinhSon <tranbavinhson@gmail.com>
2020-08-10 16:17:58 +0900

    add code

---
+++ 21_days_terraform/README.md
@@ -0,0 +1,1 @@
+Base on this [tutorial](https://medium.com/@devopslearning/21-days-of-aws-using-terraform-day-1-introduction-to-terraform-fe2cc5b4075)
--- introduction_terraform/main.tf
+++ introduction_terraform/main.tf
@@ -3,11 +3,17 @@ provider "aws" {
   region  = "ap-northeast-1"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
 # resource "<PROVIDER>_<TYPE>" "<NAME>" {
 #  [CONFIG …]
 # }
 resource "aws_instance" "example" {
-  ami           = "ami-0183ac312a029c38c" # Using Packer to create custom AMI
+  ami           = "ami-0cc75a8978fbbc969" # Using Packer to create custom AMI
   instance_type = "t2.micro"
   # <PROVIDER>_<TYPE>.<NAME>.<ATTRIBUTE>
   vpc_security_group_ids = [aws_security_group.instance.id]
@@ -15,8 +21,8 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-            EOF
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
   tags = {
     Name = "terraform-example"
   }
@@ -27,9 +33,94 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+
+# Deploy a cluster of web servers
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0cc75a8978fbbc969"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  user_data       = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_availability_zones" "all" {}
+
+# data "<PROVIDER>_<TYPE>" "<NAME>" {
+#  [CONFIG ...]
+# }
+# Access: data.<PROVIDER>_<TYPE>.<NAME>.<ATTRIBUTE>
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones   = data.aws_availability_zones.all.names
+
+  min_size = 2
+  max_size = 10
+
+  load_balancers = [aws_elb.example.name]
+  # default EC2: Only check unhealthy instance
+  # ELB: will use health check of CLB
+  health_check_type = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terrafrom-example-elb"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elb" "example" {
+  name               = "terraform-asg-example"
+  security_groups    = [aws_security_group.elb.id]
+  availability_zones = data.aws_availability_zones.all.names
+
+  health_check {
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = var.server_port
+    instance_protocol = "http"
+  }
+}
+
+output "clb_dns_name" {
+  value       = aws_elb.example.dns_name
+  description = "The domain name of the load balancer"
 }
